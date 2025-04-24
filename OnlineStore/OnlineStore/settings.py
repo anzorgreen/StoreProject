b7c0e74a584684213e8a831b41e034b2b72b06ee
Anzor Kvachantiradze <anzor.green@gmail.com>
2025-04-24 15:54:54 +0200

    tech

--- OnlineStore/OnlineStore/settings.py
+++ OnlineStore/OnlineStore/settings.py
@@ -14,7 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 4532
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
